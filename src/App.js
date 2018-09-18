9be45b6c51d1c50764b2c65d0bba6c9985f14793
Nguyenlt236 <43320074+Nguyenlt236@users.noreply.github.com>
2018-09-18 20:37:09 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./component/Header"
+import Banner from "./component/Banner"
+import CategoriesBar from "./component/Categories bar"
+import NewArrivals from "./component/New Arrivals"
+import Deals from "./component/Deals"
+import Shipping from "./component/Shipping"
+import Footer from "./component/Footer"
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Banner />
+        <CategoriesBar />
+        <NewArrivals />
+        <Deals />
+        <Shipping />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
